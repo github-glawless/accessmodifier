daea1b681b1e0fcdfff0bfb813bc80864029fccd
Lawless, Gregory <Gregory.Lawless@Nelnet.Net>
2021-06-24 17:20:48 -0400

    Access modifier exercise 2

--- Program.cs
+++ Program.cs
@@ -29,10 +29,3 @@ namespace accessmodifier
 }
 
 
-
-public class Plane {
-            public const int RowCount = 10;
-            public const int SeatPerRow = 6;
-            public const int SeatCount = RowCount * SeatPerRow;
-            public string TailNumber = "NN8042";
-            public string FlightNum
